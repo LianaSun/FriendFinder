257b1cd55c21b8164e9de648fe76fd0515996f60
Liana Sun <lianaelindgren@gmail.com>
2019-04-24 10:41:00 -0500

    saving

---
+++ server.js
@@ -0,0 +1,23 @@
+//NPM Packages Required
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+//Express Configuration
+var app = express();
+
+//PORT Listener
+var PORT = 8080;
+
+//Express data parsing
+app.use(bodyParser.urlencoded({ extended = true }));
+app.use(bodyParser.json());
+
+//Routes
+require(".app/routing/apiRoutes.js")(app);
+require(".app/routing/htmlRoutes.js")(app);
+
+//Listener
+app.listen(PORT, function(){
+    console.log("Listeningon PORT:" + PORT);
+});
