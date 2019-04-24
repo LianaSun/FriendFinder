5feaabbc33bd7c938dcb08e223dde9d54954a154
Liana Sun <lianaelindgren@gmail.com>
2019-04-24 10:47:00 -0500

    friends.js updated

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,40 @@
+//Friends Data Array
 
+var friendsArray = [
+    {
+      name: "Reese Witherspoon",
+      photo: 'https://timedotcom.files.wordpress.com/2000/04/reese-witherspoon-time-100-2015-pioneers.jpg',
+      scores: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      5,
+      4,
+      3,
+      2,
+      1
+      ]
+    },
+    {
+      name: "Mindy Kaling",
+      photo: 'http://theconcernsofmindykaling.com/wp-content/uploads/2015/08/mindy-kaling.jpg',
+      scores: [
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5
+      ]
+    }
+  ];
+  
+  // this makes the data accessible by other files
+  module.exports = friendsArray;
 
